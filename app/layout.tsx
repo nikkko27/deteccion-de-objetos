b42355570a91913a20ebac16e7889de5bb72057f
nikkko27 <79665324+nikkko27@users.noreply.github.com>
2025-02-20 23:09:12 -0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+// app/layout.tsx
+export const metadata = {
+  title: "Mi App",
+  description: "Una aplicación Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
