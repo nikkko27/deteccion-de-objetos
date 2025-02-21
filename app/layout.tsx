cd2cf491b47c3c858aaa9d98372370e0d77eeabc
nikkko27 <79665324+nikkko27@users.noreply.github.com>
2025-02-20 23:49:42 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,9 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
+export const metadata = {
   title: "Mi App",
   description: "Una aplicación Next.js",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
       <body>{children}</body>
