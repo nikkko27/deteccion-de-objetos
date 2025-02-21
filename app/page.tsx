254b7f2425b2fb9213d1925d4ceabe45417b70ac
nikkko27 <79665324+nikkko27@users.noreply.github.com>
2025-02-20 23:49:17 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
-export default function Home() {
+export default function MenuPage() {
   const router = useRouter();
 
-  useEffect(() => {
-    router.push("/login"); // Redirige automáticamente a la página de login
-  }, []);
-
-  return <p>Redirigiendo...</p>;
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <div className="bg-white p-6 rounded shadow-md text-center">
+        <h2 className="text-xl font-bold mb-4">Menú Principal</h2>
+        <button
+          onClick={() => router.push("/camara")}
+          className="w-full bg-green-500 text-white p-2 rounded mb-2"
+        >
+          Cámara en Vivo
+        </button>
+      </div>
+    </div>
+  );
 }
